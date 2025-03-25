bdbf0b2e50a0dec89de9d49dd288bbca3b9708cb
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-03-25 21:44:26 +0500

    modules

--- parsing-request/actualparsing.js
+++ parsing-request/actualparsing.js
@@ -1,6 +1,6 @@
-const http = require("http");
+
 const fs = require("fs");
-const server = http.createServer((req, res) => {
+const requestHandler = (req, res) => {
   console.log(req.url, req.method);
   if (req.url === "/") {
     res.setHeader("Content-Type", "text/html");
@@ -28,20 +28,29 @@ const server = http.createServer((req, res) => {
     req.url.toLowerCase() === "/user-details" &&
     req.method == "POST"
   ) {
-    const body = []; // elements can add in array , chunk will be add
+    const body = []; // elements can add in array , chunk will be add //store received chunk
     //reading chunk
     req.on("data", (chunk) => {
       console.log(chunk); // ui sy post request aye to tell us
       body.push(chunk);
     });
     // next event when chunk end
+    //chunk received and store in body
     req.on("end", () => {
-      //receive signal on receiving complete data
+      //receive signal on receiving complete data // extract full body of data after receiving complete chunk
       const fullBodyData = Buffer.concat(body).toString();
-      console.log(fullBodyData)
+      console.log(fullBodyData);
+      const params = new URLSearchParams(fullBodyData); // parse the body through this urlsearchparams
+      //   const bodyObj = {};
+      //   for (const [key, val] of params.entries()) {
+      //     //after extracting we have to store it , it is receiving in object form key value pair so we store it in empty object
+      //     bodyObj[key] = val;
+      //   }
+      const bodyObj = Object.fromEntries(params); // after parse make object
+      console.log(bodyObj);
+      fs.writeFileSync("user.txt", JSON.stringify(bodyObj)); //after parsing create user file //object write into file
     });
 
-    fs.writeFileSync("user.txt", "sadaf");
     res.statusCode = 302; //redirect statuscode
     res.setHeader("Location", "/"); //redirect after submitting
     return res.end();
@@ -54,9 +63,7 @@ const server = http.createServer((req, res) => {
   res.write("</body>");
   res.write("</html>");
   return res.end();
-});
+}
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+
+module.exports = requestHandler
