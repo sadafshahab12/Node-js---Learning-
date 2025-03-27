0b3a0f1b04ab08f11ea373c37bfee6a0aed92e23
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-03-27 11:50:42 +0500

    npm install done

--- Event-loop/run&observe.js
+++ Event-loop/run&observe.js
@@ -1,2 +1,35 @@
-const fs = require("fs")
-console.log(`1. Starting of Script`)
+//Blocking vs Async
+const fs = require("fs");
+// console.log(`1. Starting of Script`);
+// console.log(`2. Reading file Synchronously`);
+// const dataSync = fs.readFileSync("user-details.txt", "utf-8");
+// console.log(`3. synchronous read complete`);
+
+// console.log(`4. Reading file asynchronously`);
+// fs.readFile("user-details.txt", "utf-8", (error, dataAsync) => {
+//   if (error) throw error;
+//   console.log(`6. Asynchronous Read complete`);
+// });
+// console.log(`5. End of Script`);
+
+// Event loop sequence
+console.log(`1. Starting of Script`);
+Promise.resolve().then(() => console.log(`2. Microtask 1`));
+//timer queue
+setTimeout(() => {
+  console.log(`3. Timer 1`);
+}, 0);
+
+//I/O queue
+
+fs.readFile("user-details.txt", "utf-8", () => {
+  console.log(`4. I/O operation`);
+});
+//Check queue
+setImmediate(() => console.log(`5. Immediate 1`));
+//close queue
+
+process.on("exit", (code) => {
+  console.log(`6. Exit Event`);
+});
+console.log(`7. End of Script`);
