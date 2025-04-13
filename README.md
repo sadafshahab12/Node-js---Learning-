876e6f583024189c6add7871994ecce9d87385d7
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-04-13 21:55:41 +0500

    404 page not found with status code

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Difference from app.get() or app.post()
 - app.use() is used to define middleware that runs before route handlers.
 - app.get()/app.post() are used to define specific routes for HTTP GET or POST methods.
+
+body-parser helps Express read and understand incoming data (like form submissions or JSON).
+It’s like a translator for incoming messages.
+Newer Express versions have it built-in!
--- express router/app.js
+++ express router/app.js
@@ -9,10 +9,15 @@ app.use("/", (req, res, next) => {
   next();
 });
 app.use(express.urlencoded());
+//express router 
 app.use(userRouter);
 app.use(hostRouter);
 app.use(hostRouter);
 
+// 404 page not found with status code 
+app.use((req, res, next) => {
+  res.status(404).send(`<h1>404! page not found</h1>`);
+});
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
