b5697bea749454eae332b785f7b5d87054e92198
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-05-11 21:51:26 +0500

    learn how write and read file , data handling

--- mvc-model view controller/controllers/homes.js
+++ mvc-model view controller/controllers/homes.js
@@ -1,7 +1,7 @@
 const AddBook = require("../model/home");
 
 exports.home = (req, res, next) => {
-  const addedBooks = AddBook.fetchAll();
-  console.log(addedBooks);
-  res.render("home", { addedBooks, title: "book website" }); // render is attached with ejs
+  AddBook.fetchAll((addedBooks) => {
+    res.render("home", { addedBooks, title: "book website" });
+  });
 };
--- mvc-model view controller/model/home.js
+++ mvc-model view controller/model/home.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const rootDir = require("../utils/path_utils");
-let addedBooks = [];
+
 
 module.exports = class AddBook {
   constructor(bookName, bookAuthor, bookPrice, bookImage) {
@@ -17,16 +17,19 @@ module.exports = class AddBook {
       console.log(`File writing concluded: ${error}`);
     });
   }
-  static fetchAll() {
+  static fetchAll(callback) {
     const addBookPath = path.join(rootDir, "data", "books.json");
     fs.readFile(addBookPath, (error, data) => {
       console.log(`File Read: ${error} ${data}`);
-      if (error) {
-        console.log(error);
-        addedBooks = [];
-      } else {
+      // if (error) {
+      //   addedBooks = [];
+      // } else {
+      //   addedBooks = JSON.parse(data);
+      // }
+      if (!error) {
         addedBooks = JSON.parse(data);
       }
+      callback(addedBooks);
     });
   }
 };
