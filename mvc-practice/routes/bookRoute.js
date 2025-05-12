230cf215ad4139b03ceb7fe2fd7c781adac28d7a
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-05-12 19:25:52 +0500

    practicing mvc

---
+++ mvc-practice/routes/bookRoute.js
@@ -0,0 +1,11 @@
+import express from "express";
+const bookRouter = express.Router();
+
+bookRouter.get("/books", (req, res, next)=>{
+
+})
+
+bookRouter.post("/books", (req, res, next)=>{
+  
+})
+export {bookRouter};
