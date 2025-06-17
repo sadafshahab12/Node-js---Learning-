046a6c9d8e2081112d6a0693e857c5148ba7b10f
sadafshahab12 <sadafshahab123@yahoo.com>
2025-06-16 21:12:44 -0700

    styling done

--- dynamic-with-mvc-path-query/app.js
+++ dynamic-with-mvc-path-query/app.js
@@ -3,6 +3,8 @@ const homeRouter = require("./routes/homeRoute");
 const addBookRoute = require("./routes/addBookRoute");
 const { notFoundController } = require("./controllers/notFoundController");
 const bookRouter = require("./routes/booksRoute");
+const path = require("path");
+const rootDir = require("./utils/path_utils");
 
 const app = express();
 
@@ -14,6 +16,7 @@ app.use((req, res, next) => {
   console.log("get request url", req.url, req.method);
   next();
 });
+app.use(express.static(path.join(rootDir, "public")));
 app.use(express.urlencoded());
 app.use(homeRouter);
 app.use(addBookRoute);
